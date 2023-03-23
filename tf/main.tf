5d3632a292a72d4ae8718c48fcb4cc574a4d577a
Jeff DeLong <jeffdelong@c02fx51rml85.mynetworksettings.com>
2023-03-23 16:18:07 -0400

    initial config

--- tf/main.tf
+++ tf/main.tf
@@ -1,1 +1,10 @@
-test
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "~> 0.45.0" # Refer to docs for latest version
+    }
+  }
+}
+
+provider "auth0" {}
