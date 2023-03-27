70c27c25101dd4445f66ac2ff80ac7e683c520b4
jeffdel2 <jeffdel2@gmail.com>
2023-03-27 10:12:20 -0400

    Testing app update

--- tf/main.tf
+++ tf/main.tf
@@ -13,7 +13,7 @@ provider "auth0" {}
 
 resource "auth0_client" "my_client" {
   name            = "WebAppExampleUpdated"
-  description     = "My Web App Created Through Terraform"
+  description     = "My Web App Created Through Terraform again"
   app_type        = "regular_web"
   callbacks       = ["http://localhost:3000/callback"]
   oidc_conformant = true
